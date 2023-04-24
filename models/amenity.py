43435b9a66fe0efd2bff7830dcb34abcfceb030d
Nnamani Joel Ajuluchukwu <113372172+Nnamaniajuluchukwu@users.noreply.github.com>
2023-04-24 20:45:34 +0100

    Update amenity.py

--- models/amenity.py
+++ models/amenity.py
@@ -1,14 +1,7 @@
 #!/usr/bin/python3
-""" Amenity Module for AirBnB project """
-from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String
-from sqlalchemy.orm import relationship
+""" State Module for HBNB project """
+from models.base_model import BaseModel
 
 
-class Amenity(BaseModel, Base):
-    """ Amenity object: Inherits from BaseModel and Base (sqlalchemy) """
+class Amenity(BaseModel):
     name = ""
-#    __tablename__ = "amenities"
-#    name = Column(String(128), nullable=False)
-#    place_amenities = relationship("Place", secondary="place_amenity",
-#            viewonly=False, back_populates="amenities")
