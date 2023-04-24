e362b04839d45535e6908506c0573242f4af57ef
Nnamani Joel Ajuluchukwu <113372172+Nnamaniajuluchukwu@users.noreply.github.com>
2023-04-24 20:03:09 +0100

    Update state.py

--- models/state.py
+++ models/state.py
@@ -1,8 +1,27 @@
 #!/usr/bin/python3
-""" State Module for HBNB project """
-from models.base_model import BaseModel
+""" State module for AirBnB Clone Project """
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship, backref
+from os import getenv
+from models.city import City
 
 
-class State(BaseModel):
-    """ State class """
-    name = ""
+class State(BaseModel, Base):
+    """" State Object: inherit from BaseModel and Base (sqlalchemy) """
+    __tablename__ = "states"
+    name = Column(String(128), nullable=False)
+    cities = relationship("City", backref=backref("state", cascade="all,delete"),
+            cascade="all, delete, delete-orphan",
+            passive_deletes=True,
+            single_parent=True)
+
+    if getenv("HBNB_TYPE_STORAGE") != "db":
+        @property
+        def cities(self):
+            """ Return list of city instances with state.id. """
+            # This prevent circular import.
+            from models import storage
+            cities_ = [obj for obj in storage.all(City).values()
+            if obj.state_id == self.id]
+                return cities_
