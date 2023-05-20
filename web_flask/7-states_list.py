84462adaee6d33681cf7d646cc7a3462c685d84c
Nnamani Joel Ajuluchukwu <113372172+Nnamaniajuluchukwu@users.noreply.github.com>
2023-05-20 10:14:03 +0100

    Add files via upload

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""Your web application must be listening on 0.0.0.0, port 5000
+You must use storage for fetching data from the storage engine
+(FileStorage or DBStorage) => from models import storage and storage.all(...)
+After each request you must remove the current SQLAlchemy Session:
+    Declare a method to handle @app.teardown_appcontext
+    Call in this method storage.close()
+    Routes:
+    /states_list: display a HTML page: (inside the tag BODY)
+    H1 tag: “States”
+    UL tag: with the list of all State objects present in DBStorage sorted by
+    name (A->Z) tip
+    LI tag: description of one State: <state.id>: <B><state.name></B>
+"""
+from flask import Flask, render_template
+from models import storage
+
+app = Flask(__name__)
+
+
+@app.route('/states_list', strict_slashes=False)
+def states_list():
+    """display a HTML page with the states listed in alphabetical order"""
+    all_states = storage.all('State')
+    states = list()
+    for state in all_states.values():
+        states.append(state)
+    return render_template('7-states_list.html', states=states)
+
+
+@app.teardown_appcontext
+def teardown_db(exception):
+    """closes the storage on teardown"""
+    storage.close()
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port='5000')
